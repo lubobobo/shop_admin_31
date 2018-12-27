a4386807558b3d356daa336b88c4331fd0a9ff8f
lubobobo <2625589107@qq.com>
2018-12-27 19:34:08 +0800

    初始化提交

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">这是home组件</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login">
+    <div class="login-box"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+// // lang="less": 表示使用less写css
+// .login {
+//   width: 100%;
+//   height: 100%;
+//   background-color: #2D434C;
+//   overflow: hidden;
+//   .login-box {
+//     width: 400px;
+//     height: 300px;
+//     background-color: #fff;
+//     margin: 200px auto;
+//   }
+// }
+</style>
